8841adc8682e5f9557a76a33833dfdb1b05add79
Ruslan Absalyamov <ryslan.absalam@gmail.com>
2023-09-10 00:08:05 +0600

    Beginning of the client part

--- Core/Specification/BaseSpecification.cs
+++ Core/Specification/BaseSpecification.cs
@@ -24,5 +24,33 @@ namespace Core.Specification
             Includes.Add(includeExpression);
         }
 
+        public Expression<Func<T, object>> OrderBy { get; private set; }
+
+        public Expression<Func<T, object>> OrderByDesc { get; private set; }
+
+        public int Take { get; private set; }
+
+        public int Skip { get; private set; }
+
+        public bool IsPagingEnabled { get; private set; }
+
+
+        protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
+        {
+            OrderBy = orderByExpression;
+        }
+
+        protected void ApplyPaging(int skip, int take)
+        {
+            Take = take;
+            Skip = skip;
+            IsPagingEnabled = true;
+        }
+
+        protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
+        {
+            OrderByDesc = orderByDescExpression;
+        }
+
     }
 }
